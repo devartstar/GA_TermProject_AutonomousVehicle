29407071567f09b02f2cf13537a93125c049c922
Devjit_Choudhury <61665451+devartstar@users.noreply.github.com>
2021-04-15 23:30:00 +0530

    Update nn.js

--- nn.js
+++ nn.js
@@ -1,7 +1,4 @@
-// Daniel Shiffman
-// Neuro-Evolution Flappy Bird with TensorFlow.js
-// http://thecodingtrain.com
-// https://youtu.be/cdUNkwXx-I4
+
 
 class NeuralNetwork {
   constructor(a, b, c, d) {
